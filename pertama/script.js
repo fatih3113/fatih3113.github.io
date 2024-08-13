8221fdceede87e62ff52f5c78b5f598e0e2c24be
fatih3113 <fatihmubarok850@gmail.com>
2024-08-13 21:42:32 +0700

    Add files via upload

---
+++ pertama/script.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.getElementById('nav-links').classList.toggle('active');
+});
